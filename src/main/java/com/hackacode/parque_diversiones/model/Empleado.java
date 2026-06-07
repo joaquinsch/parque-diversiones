ca232d9cacfe224ec276e1ea7aba5f085625e7f3
joaquinsch <joacohao1@gmail.com>
2026-06-07 18:48:10 -0300

    empleado tiene una lista de asignaciones, que muestra los juegos a los que está asociado

--- src/main/java/com/hackacode/parque_diversiones/model/Empleado.java
+++ src/main/java/com/hackacode/parque_diversiones/model/Empleado.java
@@ -3,8 +3,11 @@ package com.hackacode.parque_diversiones.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter @Setter
 public abstract class Empleado {
     private Long id_empleado;
     private String nombre;
+    private List<Asignacion> asignaciones;
 }
--- src/test/java/com/hackacode/parque_diversiones/model/EmpleadoTests.java
+++ src/test/java/com/hackacode/parque_diversiones/model/EmpleadoTests.java
@@ -4,24 +4,38 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmpleadoTests {
     Empleado empleado;
 
     @BeforeEach
     void setUp() {
+        empleado = new EmpleadoJuego();
     }
 
     @Test
     public void deberiaCrearseConId() {
-        empleado = new EmpleadoJuego();
         empleado.setId_empleado(1L);
         Assertions.assertEquals(1L, empleado.getId_empleado());
     }
 
     @Test
     public void deberiaCrearseConNombre() {
-        Empleado empleado = new EmpleadoJuego();
         empleado.setNombre("carlos");
         Assertions.assertEquals("carlos", empleado.getNombre());
+    }
+
+    @Test
+    public void deberiaTenerUnaListaDeAsignaciones() {
+        Asignacion asignacion = new Asignacion();
+        asignacion.setEmpleado(empleado);
+
+        List<Asignacion> asignaciones = new ArrayList<>();
+        asignaciones.add(asignacion);
+        empleado.setAsignaciones(asignaciones);
+        Assertions.assertNotNull(empleado.getAsignaciones());
+        Assertions.assertEquals(1, empleado.getAsignaciones().size());
     }
 }

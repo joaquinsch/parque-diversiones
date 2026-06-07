a91adb83d50703cf052d3588480e7c0a1939a8c3
joaquinsch <joacohao1@gmail.com>
2026-06-07 01:54:09 -0300

    primer commit

---
+++ src/main/java/com/hackacode/parque_diversiones/ParqueDiversionesApplication.java
@@ -0,0 +1,13 @@
+package com.hackacode.parque_diversiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueDiversionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueDiversionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackacode/parque_diversiones/ParqueDiversionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackacode.parque_diversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueDiversionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

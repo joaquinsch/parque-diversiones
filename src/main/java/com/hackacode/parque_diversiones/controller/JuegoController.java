44313ffe374051525320d6abe7bb35bb3c62dc31
joaquinsch <joacohao1@gmail.com>
2026-06-09 00:14:53 -0300

    refactor: uso exception handler para manejar excepciones

--- src/main/java/com/hackacode/parque_diversiones/controller/JuegoController.java
+++ src/main/java/com/hackacode/parque_diversiones/controller/JuegoController.java
@@ -1,7 +1,7 @@
 package com.hackacode.parque_diversiones.controller;
 
 import com.hackacode.parque_diversiones.dto.JuegoDTO;
-import com.hackacode.parque_diversiones.exceptions.HorarioInvalidoError;
+import com.hackacode.parque_diversiones.dto.JuegoResponseDTO;
 import com.hackacode.parque_diversiones.service.IJuegoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/juegos")
@@ -22,14 +20,9 @@ public class JuegoController {
     }
 
     @PostMapping
-    public ResponseEntity<?> guardarJuego(@RequestBody JuegoDTO juegoDTO) {
-        try {
-            return new ResponseEntity<>(juegoService.guardarJuego(juegoDTO), HttpStatus.CREATED);
-
-        } catch (HorarioInvalidoError e) {
-            return ResponseEntity.
-                    badRequest()
-                    .body(Map.of("mensaje", e.getMessage()));
-        }
+    public ResponseEntity<JuegoResponseDTO> guardarJuego(@RequestBody JuegoDTO juegoDTO) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(juegoService.guardarJuego(juegoDTO));
     }
 }
---
+++ src/main/java/com/hackacode/parque_diversiones/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.hackacode.parque_diversiones.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HorarioInvalidoError.class)
+    public ResponseEntity<Map<String, String>> handleHorarioInvalidoError(HorarioInvalidoError e) {
+        return ResponseEntity
+                .badRequest()
+                .body((Map.of("mensaje", e.getMessage())));
+    }
+}
